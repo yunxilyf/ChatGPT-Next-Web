e502f71e3ecec0faaa7a9a92aef2939dfb663a52
H0llyW00dzZ <priv8@btz.pm>
2023-11-07 05:31:11 +0700

    Feat Models [GPT-3.5 1106] (#58)
    [+] feat(constant.ts): add new model gpt-3.5-turbo-1106 to DEFAULT_MODELS

--- app/constant.ts
+++ app/constant.ts
@@ -133,6 +133,10 @@ export const DEFAULT_MODELS = [
     available: true,
   },
   {
+    name: "gpt-3.5-turbo-1106",
+    available: true,
+  },
+  {
     name: "gpt-3.5-turbo-16k",
     available: true,
   },
