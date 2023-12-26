acc489fce6553a63e09ef24b4c05447bdd60e1e8
H0llyW00dzZ <priv8@btz.pm>
2023-12-26 21:31:46 +0700

    Fix [UI/UX] [Model Config] Duplicate (#209)
    - [+] refactor(model-config.tsx): remove unused code for input template and history count

--- app/components/model-config.tsx
+++ app/components/model-config.tsx
@@ -315,39 +315,6 @@ export function ModelConfigList(props: {
           </ListItem>
 
           <ListItem
-            title={Locale.Settings.InputTemplate.Title}
-            subTitle={Locale.Settings.InputTemplate.SubTitle}
-          >
-            <input
-              type="text"
-              value={props.modelConfig.template}
-              onChange={(e) =>
-                props.updateConfig(
-                  (config) => (config.template = e.currentTarget.value),
-                )
-              }
-            ></input>
-          </ListItem>
-
-          <ListItem
-            title={Locale.Settings.HistoryCount.Title}
-            subTitle={Locale.Settings.HistoryCount.SubTitle}
-          >
-            <InputRange
-              title={props.modelConfig.historyMessageCount.toString()}
-              value={props.modelConfig.historyMessageCount}
-              min="0"
-              max="64"
-              step="1"
-              onChange={(e) =>
-                props.updateConfig(
-                  (config) => (config.historyMessageCount = e.target.valueAsNumber),
-                )
-              }
-            ></InputRange>
-          </ListItem>
-
-          <ListItem
             title={Locale.Settings.CompressThreshold.Title}
             subTitle={Locale.Settings.CompressThreshold.SubTitle}
           >
