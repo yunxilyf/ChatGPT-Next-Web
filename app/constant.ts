038570cac2573bbce1e276f7c895c642c30d3f7b
H0llyW00dzZ <priv8@btz.pm>
2024-02-07 12:19:57 +0700

    Update Git & Docker Ignore (#250)
    * Update git ignore
    
    - [+] feat(.gitignore): add new files to ignore list
    - [+] feat(app/constant.ts): add gemini-pro-vision to DEFAULT_MODELS array
    
    * Update docker ignore
    
    - [+] feat(.dockerignore): add more files and directories to be ignored during Docker image build

--- app/constant.ts
+++ app/constant.ts
@@ -335,6 +335,15 @@ export const DEFAULT_MODELS = [
       providerType: "google",
     },
   },
+  {
+    name: "gemini-pro-vision",
+    available: false, // not yet ready
+    provider: {
+      id: "google",
+      providerName: "Google",
+      providerType: "google",
+    },
+  },
 ] as const;
 
 export const CHAT_PAGE_SIZE = 15;
